01ed042fbdf4a9fac787be2772f68af308583e97
mehdibenfakhta <mehdi.benfakhta@gmail.com>
2024-07-31 13:45:52 +0100

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -77,6 +77,7 @@ const routes: Routes = [
   {path:'forgot-password', component: ForgotPasswordComponent},
   {path: 'unauthorized',component:UnauthorizedComponent},
   { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login', pathMatch: 'full' },
 ];
 
 @NgModule({
---
+++ src/app/mehdi/mehdi.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mehdi',
+  standalone: true,
+  imports: [],
+  templateUrl: './mehdi.component.html',
+  styleUrl: './mehdi.component.scss'
+})
+export class MehdiComponent {
+
+}
