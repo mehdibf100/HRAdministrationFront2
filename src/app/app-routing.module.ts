16b0f367cd9b50f512dcd70684dce5a34453b91d
AmineHammami17 <lorenzoamine17@gmail.com>
2024-08-02 20:24:41 +0100

    Commit 02/08/2024

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,6 @@
-// Angular Import
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-// project import
 import { AdminComponent } from './theme/layout/admin/admin.component';
-import { GuestComponent } from './theme/layout/guest/guest.component';
 import path from 'path';
 import { LoginComponent } from './login/login.component';
 import { DashboardAdminComponent } from './dashboard-admin/dashboard-admin.component';
@@ -21,7 +17,7 @@ import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
 const routes: Routes = [
   {
     path: '',
-    component: AdminComponent,canActivate: [AuthGuard , AdminGuard],
+    component: AdminComponent,
     children: [
       {
         path: 'admin-dashboard',
@@ -37,38 +33,16 @@ const routes: Routes = [
         loadChildren: () => import('./demo/ui-element/ui-basic.module').then((m) => m.UiBasicModule)
       },
       {
-        path: 'chart',
-        loadComponent: () => import('./demo/chart & map/core-apex.component')
-      },
-      {
         path: 'forms',
         loadComponent: () => import('./demo/forms & tables/form-elements/form-elements.component')
       },
       {
         path: 'tables',
         loadComponent: () => import('./demo/forms & tables/tbl-bootstrap/tbl-bootstrap.component')
-      },
-      {
-        path: 'sample-page',
-        loadComponent: () => import('./demo/sample-page/sample-page.component')
       }
     ]
   },
-  {
-    path: '',
-    component: GuestComponent,
-    children: [
-      {
-        path: 'auth/signup',
-        loadComponent: () => import('./demo/authentication/sign-up/sign-up.component')
-      },
-      {
-        path: 'auth/signin',
-        loadComponent: () => import('./demo/authentication/sign-in/sign-in.component')
-      }
-    ]
-  },
-  { path: 'login', component: LoginComponent },
+
   { path: 'login', component: LoginComponent },
  // { path: 'adminb', component: DashboardAdminComponent   },
   { path: 'admin-hr-dashboard', component: DashboardHrComponent, canActivate: [AuthGuard , AdminHRGuard] },
@@ -77,7 +51,6 @@ const routes: Routes = [
   {path:'forgot-password', component: ForgotPasswordComponent},
   {path: 'unauthorized',component:UnauthorizedComponent},
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: '**', redirectTo: '/login', pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './theme/layout/admin/admin.component';
-import { GuestComponent } from './theme/layout/guest/guest.component';
-import { ConfigurationComponent } from './theme/layout/admin/configuration/configuration.component';
 import { NavBarComponent } from './theme/layout/admin/nav-bar/nav-bar.component';
 import { NavigationComponent } from './theme/layout/admin/navigation/navigation.component';
 import { NavLeftComponent } from './theme/layout/admin/nav-bar/nav-left/nav-left.component';
-import { NavRightComponent } from './theme/layout/admin/nav-bar/nav-right/nav-right.component';
 import { NavSearchComponent } from './theme/layout/admin/nav-bar/nav-left/nav-search/nav-search.component';
-import { ChatMsgComponent } from './theme/layout/admin/nav-bar/nav-right/chat-msg/chat-msg.component';
-import { ChatUserListComponent } from './theme/layout/admin/nav-bar/nav-right/chat-user-list/chat-user-list.component';
-import { FriendComponent } from './theme/layout/admin/nav-bar/nav-right/chat-user-list/friend/friend.component';
 import { NavContentComponent } from './theme/layout/admin/navigation/nav-content/nav-content.component';
 import { NavCollapseComponent } from './theme/layout/admin/navigation/nav-content/nav-collapse/nav-collapse.component';
 import { NavGroupComponent } from './theme/layout/admin/navigation/nav-content/nav-group/nav-group.component';
@@ -35,16 +29,10 @@ import { HttpClientModule } from '@angular/common/http';
   declarations: [
     AppComponent,
     AdminComponent,
-    GuestComponent,
-    ConfigurationComponent,
     NavBarComponent,
     NavigationComponent,
     NavLeftComponent,
-    NavRightComponent,
     NavSearchComponent,
-    ChatMsgComponent,
-    ChatUserListComponent,
-    FriendComponent,
     NavContentComponent,
     NavItemComponent,
     NavCollapseComponent,
